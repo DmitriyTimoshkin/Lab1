e82c6ed0c220a2de31d120d7cda1bd1f1504f824
Administrator <admin@example.com>
2022-03-30 21:11:08 +0400

    deikstra perkol

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# import random
 import numpy as np
+import secrets
 import pygame
 import sys
+import time
 
 run = True
 
-FPS = 12000000000000000000000000000
+FPS = 120000000000000000000000000000
+fps = 60
 
-side = 1
-WIDTH = 1000
-HEIGHT = 1000
+side = 20
+WIDTH = 20
+HEIGHT = 20
 
 WHITE = (255, 255, 255)
+GRAY = (175, 175, 175)
 BLACK = (0, 0, 0)
 
 RED = (255, 0, 0)
@@ -23,59 +30,142 @@ screen = pygame.display.set_mode((WIDTH * side, HEIGHT * side))
 clock = pygame.time.Clock()
 pygame.mouse.set_visible(False)
 
-par = 1
-flag = True
-
-
-def temp(param):
-    return np.random.standard_gamma(param, size=(WIDTH, HEIGHT)) #np.random.randint(param, size=(WIDTH, HEIGHT))
-
 
 def workWithArr(arr):
-    global flag
     pos = pygame.mouse.get_pos()
     pressed = pygame.mouse.get_pressed()
 
     if pygame.mouse.get_focused():
-        pygame.draw.rect(screen, RED, ((pos[0] - side/2) - (pos[0] - side/2) %
-                         side, (pos[1] - side/2) - (pos[1] - side/2) % side, side, side))
+        pygame.draw.rect(screen, RED, ((pos[0] - side / 2) - (pos[0] - side / 2) %
+                                       side, (pos[1] - side / 2) - (pos[1] - side / 2) % side, side, side))
 
     if pressed[0]:
-        flag = True
-        arr[int((pos[1] - side/2) - (pos[1] - side/2) % side) // side,
-            int((pos[0] - side/2) - (pos[0] - side/2) % side) // side] = 1
-    elif pressed[1]:
-        flag = True
-        arr = temp(par)
+        arr[int((pos[1] - side / 2) - (pos[1] - side / 2) % side) // side,
+            int((pos[0] - side / 2) - (pos[0] - side / 2) % side) // side] = 1
     elif pressed[2]:
-        flag = True
-        arr[int((pos[1] - side/2) - (pos[1] - side/2) % side) // side,
-            int((pos[0] - side/2) - (pos[0] - side/2) % side) // side] = 0
+        arr[int((pos[1] - side / 2) - (pos[1] - side / 2) % side) // side,
+            int((pos[0] - side / 2) - (pos[0] - side / 2) % side) // side] = 0
 
     return arr
 
 
+def klust_count(arr):
+    klust_arr = []
+    total = []
+    kurwa = np.zeros((WIDTH, WIDTH))
+    for i in range(WIDTH):
+        for j in range(WIDTH):
+            if arr[i][j] == 0 and kurwa[i][j] == 0:
+                kurwa[i][j] = 1
+            elif arr[i][j] == 1 and kurwa[i][j] == 0:
+                pass
+
+
+def deikstra(arr):
+    wae = np.full((WIDTH, WIDTH), FPS)  # do you know de wae
+    proi_wae = np.zeros((WIDTH, WIDTH))  # you know de wae
+    for i in range(WIDTH):
+        wae[0][i] = 0
+    cumzone = np.cumsum(proi_wae[WIDTH - 1])
+    while cumzone[-1] < (WIDTH * FPS) * 0.95:
+        prom_arr = wae + proi_wae
+        a = np.argmin(prom_arr)
+        y = a // WIDTH
+        x = a % WIDTH
+        if x - 1 >= 0 and proi_wae[y][x - 1] == 0:
+            if (wae[y][x] + (WIDTH ** 2 + 1) ** (1 - arr[y][x - 1]) < wae[y][x - 1]):
+                wae[y][x - 1] = wae[y][x] + (WIDTH ** 2 + 1) ** (1 - arr[y][x - 1])
+        if x + 1 <= WIDTH - 1 and proi_wae[y][x + 1] == 0:
+            if (wae[y][x] + (WIDTH ** 2 + 1) ** (1 - arr[y][x + 1]) < wae[y][x + 1]):
+                wae[y][x + 1] = wae[y][x] + (WIDTH ** 2 + 1) ** (1 - arr[y][x + 1])
+        if y - 1 >= 0 and proi_wae[y - 1][x] == 0:
+            if (wae[y][x] + (WIDTH ** 2 + 1) ** (1 - arr[y - 1][x]) < wae[y - 1][x]):
+                wae[y - 1][x] = wae[y][x] + (WIDTH ** 2 + 1) ** (1 - arr[y - 1][x])
+        if y + 1 <= WIDTH - 1 and proi_wae[y + 1][x] == 0:
+            if (wae[y][x] + (WIDTH ** 2 + 1) ** (1 - arr[y + 1][x]) < wae[y + 1][x]):
+                wae[y + 1][x] = wae[y][x] + (WIDTH ** 2 + 1) ** (1 - arr[y + 1][x])
+        proi_wae[y][x] = FPS
+        cumzone = np.cumsum(proi_wae[WIDTH - 1])
+    # print(wae)
+    x = np.argmin(wae[WIDTH - 1])
+    y = WIDTH - 1
+    omega_wae = []
+    omega_wae.append([y, x])
+    while y != 0:
+        if x - 1 >= 0:
+            if wae[y][x - 1] == wae[y][x] - (WIDTH ** 2 + 1) ** (1 - arr[y][x]):
+                x = x - 1
+                omega_wae.append([y, x])
+        if x + 1 <= WIDTH - 1:
+            if wae[y][x + 1] == wae[y][x] - (WIDTH ** 2 + 1) ** (1 - arr[y][x]):
+                x = x + 1
+                omega_wae.append([y, x])
+        if y - 1 >= 0:
+            if wae[y - 1][x] == wae[y][x] - (WIDTH ** 2 + 1) ** (1 - arr[y][x]):
+                y = y - 1
+                omega_wae.append([y, x])
+        if y + 1 <= WIDTH - 1:
+            if wae[y + 1][x] == wae[y][x] - (WIDTH ** 2 + 1) ** (1 - arr[y][x]):
+                y = y + 1
+                omega_wae.append([y, x])
+
+    mass = 0
+    for a in omega_wae[:-2]:
+        mass = mass + (WIDTH ** 2 + 1) ** (1 - arr[a[0]][a[1]])
+    return omega_wae
+
+
+def init():
+    arr = np.ndarray((WIDTH, HEIGHT))
+    param = 59  # 59
+    for i, e in enumerate(arr):
+        for j, ee in enumerate(arr[i]):
+            arr[i][j] = secrets.randbelow(100)  # random.randint(0, 99) #  
+            if arr[i][j] > param:
+                arr[i][j] = 0
+            else:
+                arr[i][j] = 1
+    return arr
+
+
 def main():
-    arr = temp(par)
-    global flag
+    arr = init()
+    arr2d2 = np.zeros(shape=(WIDTH, HEIGHT))
+
+    sttime = time.time()
+
+    f = False
+    per = deikstra(arr)
     while run:
         for i in pygame.event.get():
             if i.type == pygame.QUIT:
                 sys.exit(0)
 
-        if (flag):
-            flag = False
-            screen.fill(WHITE)
-            for i, y in enumerate(arr):
-                for j, x in enumerate(y):
-                    if x < 0.59:
-                        pygame.draw.rect(
-                            screen, (0, 0, 0), (j*side, i*side, side, side))
+        screen.fill(WHITE)
+
+        for i, y in enumerate(arr):
+            for j, x in enumerate(y):
+                if x:
+
+                    pygame.draw.rect(
+                        screen, BLACK, (j * side, i * side, side, side))
+                else:
+                    arr2d2[i][j] = 1
 
         arr = workWithArr(arr)
+        crtime = time.time()
+        if crtime - sttime > 1:
+            per = deikstra(arr)
+            # f = True
+
+        for i, el in enumerate(per):
+            if not arr[el[0], el[1]]:
+                pygame.draw.rect(screen, (255, 100, 0), (el[1] * side, el[0] * side, side, side))
+            else:
+                pygame.draw.rect(screen, (255, 255, 0), (el[1] * side, el[0] * side, side, side))
 
         pygame.display.update()
-        clock.tick(FPS)
+        clock.tick(fps)
 
 
 if __name__ == '__main__':
